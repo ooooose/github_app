b91f2b14d031df6999d65ca8a38ea3cdf3d61f4d
ooooose <yuuki.oose1990@gmail.com>
2026-04-26 00:11:20 +0900

    [fix]skeleton調整

--- src/features/github/components/repo-detail-skeleton.tsx
+++ src/features/github/components/repo-detail-skeleton.tsx
@@ -13,8 +13,6 @@ export const RepoDetailSkeleton = () => {
 
       <div className="mt-6 space-y-2">
         <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-[88%]" />
-        <Skeleton className="h-4 w-[76%]" />
       </div>
 
       <div className="mt-6 flex flex-wrap gap-6">
